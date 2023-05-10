19d69d6809db63e154c8dceb6e02eae051592814
Simon Caroline <caroline.simon@bbcag.ch>
2023-05-10 16:38:00 +0200

    bmw, news done

--- pages/brands/bmw.js
+++ pages/brands/bmw.js
@@ -17,11 +17,11 @@ export default function () {
         <h2 className={styles.model}>Supersoprt</h2>
         <div className={styles.motos}>
             <div className={styles.bmwr1250rs}>
-                <img src="/bmwr1250rs" alt="R 1250 RS" width={700} />
+                <img src="/bmwr1250rs.jpg" alt="R 1250 RS" width={800} />
                 <h3 className={styles.name}>R 1250 RS</h3>
             </div>
             <div className={styles.bmws1000rr}>
-                <img src="/bmws1000rr.jpg" alt="S 1000 RR" width={700} />
+                <img src="/bmws1000rr.jpg" alt="S 1000 RR" width={800} />
                 <h3 className={styles.name}>S 1000 RR</h3>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import styles from "/style/index.module.css"
+
 export default function IndexPage() {
-    return (
-        <>
-        helo
-        </>
-    )
+    return (<>
+        <h2>News & Events</h2>
+        <div className={styles.news}>
+            <div>
+                <h4>Titel</h4>
+                <img src="https://placeholder.com/350x200" alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quo minus tenetur odit nulla quia odio, aliquid eos, illo possimus consequatur nesciunt totam. Temporibus ex libero repudiandae suscipit amet tempora iure consequuntur rerum quos dicta enim officiis possimus iste odit at, sunt asperiores eius, natus quaerat hic modi vero! Tenetur?</p>
+                </div>
+            <div>
+            <h4>Titel</h4>
+                <img src="https://placeholder.com/350x200" alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quo minus tenetur odit nulla quia odio, aliquid eos, illo possimus consequatur nesciunt totam. Temporibus ex libero repudiandae suscipit amet tempora iure consequuntur rerum quos dicta enim officiis possimus iste odit at, sunt asperiores eius, natus quaerat hic modi vero! Tenetur?</p>  
+            </div>
+            <div>
+            <h4>Titel</h4>
+                <img src="https://placeholder.com/350x200" alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quo minus tenetur odit nulla quia odio, aliquid eos, illo possimus consequatur nesciunt totam. Temporibus ex libero repudiandae suscipit amet tempora iure consequuntur rerum quos dicta enim officiis possimus iste odit at, sunt asperiores eius, natus quaerat hic modi vero! Tenetur?</p>
+            </div>
+        </div>
+    </>)
 }
